09707025796ee7d7577a2da33197504494f724bb
Nisha Patel <nishapatel@Nishas-MBP.localdomain>
2017-10-11 22:00:09 -0500

    removed email and password

--- live streaming/mail.py
+++ live streaming/mail.py
@@ -6,13 +6,13 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
 # Email you want to send the update from (only works with gmail)
-fromEmail = 'kp8523754@gmail.com'
+fromEmail = 'fromEmail@gmail.com'
 # You can generate an app password here to avoid storing your password in plain text
 # https://support.google.com/accounts/answer/185833?hl=en
-fromEmailPassword = 'Redapple0?'
+fromEmailPassword = 'fromEmailPassword'
 
 # Email you want to send the update to
-toEmail = 'nis.patel23@gmail.com'
+toEmail = 'toemail'
 def sendEmail(image):
 	msgRoot = MIMEMultipart('related')
 	msgRoot['Subject'] = 'Security Update'
