0fd4240474a2663de838553b55ba2ea9f9089be7
Ibrahem Alahdal <ibralah@ut.utm.edu>
2017-11-07 00:44:59 -0600

    Lights
    Added motion triggered lights

--- doorbell/PIR_Doorbell.py
+++ doorbell/PIR_Doorbell.py
@@ -1,7 +1,10 @@
 from gpiozero import MotionSensor
 import pygame
 from time import sleep
+import pigpio
+import RPi.GPIO as GPIO
 
+pi = pigpio.pi()
 
 pir = MotionSensor(23)
 
@@ -9,9 +12,23 @@ pygame.init()
 pygame.mixer.init()
 
 door = pygame.mixer.Sound("piano2.wav")
-while True:
-    pir.wait_for_motion()
-    print("Motion detected!")
-    door.play()
-    sleep(2)
-    pir.wait_for_no_motion()
+try:
+    while True:
+        pir.wait_for_motion()
+        print("Motion detected!")
+        pi.set_PWM_dutycycle(17, 255)
+        pi.set_PWM_dutycycle(22, 255)
+        pi.set_PWM_dutycycle(24, 255)
+        for i in range(0, 3):
+            door.play()
+            sleep(7)
+        pir.wait_for_no_motion()
+        pi.set_PWM_dutycycle(17, 0)
+        pi.set_PWM_dutycycle(22, 0)
+        pi.set_PWM_dutycycle(24, 0)
+        sleep(4)
+except KeyboardInterrupt:
+    pi.set_PWM_dutycycle(17, 0)
+    pi.set_PWM_dutycycle(22, 0)
+    pi.set_PWM_dutycycle(24, 0)
+    GPIO.cleanup()
